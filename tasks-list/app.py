dc0b3911e8490973781ad0d771fde778be513e53
EliasOliver21 <eliasoliveiradapaz@hotmail.com>
2025-04-10 16:30:33 -0300

    add: create tasks

--- tasks-list/app.py
+++ tasks-list/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from task import Task
 
 app = Flask(__name__)
@@ -7,10 +7,19 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("criar")
+task_list = {}
+
+@app.route("/criar", methods=['POST'])
 def create_task():
-    task = Task(name, description, precedency)
-    return 
+
+    name = request.form['name']
+    description = request.form['description']
+
+    task = Task(name,description)
+
+    task_list[name] = task
+
+    return f"A tarefa {name} foi cadastrada com sucesso, com a descrição {description}."
 
 
 if __name__ == "__main__":
--- tasks-list/task.py
+++ tasks-list/task.py
@@ -1,5 +1,6 @@
 class Task:
-      def __init__(self, name, description, precedency):
+      def __init__(self, name, description, precedency = None,subsequence = None):
         self.name = name
         self.description = description
         self.precedency = precedency
+        self.subsequence = subsequence
