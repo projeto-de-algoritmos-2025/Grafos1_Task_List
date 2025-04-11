fa99b189cdcdbae4f88f3c73d4605c2c2b73355a
claudiohsc <claudio885henrique@gmail.com>
2025-04-11 01:02:07 -0300

    add: alterando logica do objeto task + input de dependencias

--- tasks-list/task.py
+++ tasks-list/task.py
@@ -1,7 +1,7 @@
 class Task:
-      def __init__(self,id, name, description, precedency = None,subsequence = None):
+      def __init__(self,id, name, description, dependencies = None):
+        self.id = id
         self.name = name
         self.description = description
-        self.precedency = precedency
-        self.subsequence = subsequence
-        self.id = id
+        self.dependencies = dependencies or [] # se for None, vira []
+        
