5fd9e6e9882b87923fb5889cf35b6db970aaef67
Elias_Oliveira <eliasoliveiradapaz@hotmail.com>
2025-04-17 15:52:38 -0300

    add: topological ordering.
    Co-authored-by: claudiohsc <claudio885henrique@gmail.com>

--- tasks-list/topological_ordering.py
+++ tasks-list/topological_ordering.py
@@ -1,30 +1,31 @@
+from write_read_tasks import read_task_json
+
 
 def topological_sort(task_list):
     # cada chave é uma tarefa e os valores são as tarefas que dependem dela
     graph = {}
     # Dicionário com o número de dependências (grau de entrada-"setas chegando no nó") de cada tarefa
     in_degree = {}
+    #Lista de candidatos com grau 0
+    queue = []
+    #Lista da ordenação topológica
+    sorted_tasks = []
 
     #inicializando o grafo e os graus de entrada
     for task in task_list.values():
         graph[task.id] = []
         in_degree[task.id] = 0
-    
-    #: fila de tarefas que não têm dependências
-    queue = []
 
-    #Alimenta o dicionário graph e in_degree
-    for id, task in task_list.items():
-        graph[task.id] = {"dependencies": task.dependencies}
-        node_deps = task.dependencies.__len__()
-        in_degree[task.id] = {"node_dependencies": node_deps}
+    # Cria chves com o ids dos nós e os nós que dependem deste
+    for task in task_list.values():
+        for dep in task.dependencies:
+            graph[dep].append(task.id)
+            in_degree[task.id] += 1
 
-        if not(task.dependencies):
-            queue.append(task.id)
+    # Cria um array com os nós candidatos com grau 0
+    queue = [task_id for task_id, deg in in_degree.items() if deg == 0]
 
-    sorted_tasks = []
-
-    # TODO: Processar a fila e construir a ordenação topológica
+    # Realiza um loop que faz a ordenação topológica decrementando uma unidade dos nós dependentes do nó candidato sendo iterado
     while queue:
         current = queue.pop(0)
         sorted_tasks.append(current)
