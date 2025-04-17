de502960e668b961dfe491ef6c2a6d22cc188094
Elias_Oliveira <eliasoliveiradapaz@hotmail.com>
2025-04-17 12:19:04 -0300

    add: counting nodes that not has dependencies

--- tasks-list/topological_ordering.py
+++ tasks-list/topological_ordering.py
@@ -12,17 +12,27 @@ def topological_sort(task_list):
         graph[task.id] = []
         in_degree[task.id] = 0
 
-    #### criar um for para preencher o graph(os IDs de dependencias de cada task sendo a CHAVE e o VALOR sendo as tarefas que dependem desse ID)
-    ### ao final somar 1 no valor do ID da tarefa no in_degree 
-    for task in task_list.items():
+    # TODO: criar um for para preencher o graph(os IDs de dependencias de cada task sendo a CHAVE e o VALOR sendo as tarefas que dependem desse ID)
+    ### ao final somar 1 no valor do ID da tarefa no in_degree
+    
+
+    sorted_tasks = []
+
+    for id, task in task_list.items():
         graph[task.id] = {"dependencies": task.dependencies}
+        node_deps = task.dependencies
         node_deps = node_deps.__len__()
         in_degree[task.id] = {"node_dependencies": node_deps}
+
+        if not(task.dependencies):
+            sorted_tasks.append(task.id)
+
+   
 
 
     # TODO: fila de tarefas que não têm dependências
     
-    sorted_tasks = []
+    
 
     # TODO: Processar a fila e construir a ordenação topológica
 
